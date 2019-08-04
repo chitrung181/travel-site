e0a30d140fdb323c9e198fca7800f7bd52519817
Trung <chitrung181@gmail.com>
2019-08-04 23:38:26 +0700

    43: Completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person';
-// alert("ABC rew");
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + ' now owes xxx$ in taxes. ');
-    }
-}
+import MobileMenu from './modules/MobileMenu';
 
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var cat = new Adult("Cat McKitty", "black");
-cat.greet();
-cat.payTaxes();
-
-$('h1').remove();
+var mobileMenu = new MobileMenu();
